fd251b6b09a65ad500f04eb760b5393a3fa65e4f
poo <347547721@qq.com>
2017-03-19 15:05:24 +0800

    First commit.

--- configs/common/SysPaths.py
+++ configs/common/SysPaths.py
@@ -50,7 +50,7 @@ def system():
         try:
                 path = env['M5_PATH'].split(':')
         except KeyError:
-                path = [ '../dist/m5/system', '/n/poolfs/z/dist/m5/system' ]
+                path = [ '/dist/m5/system', '../gem5_extras/system' ]
 
         for system.dir in path:
             if os.path.isdir(system.dir):
